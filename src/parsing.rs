f84dee9c6b908831138d60fb339d7d33d60e5f7a
Cattimus <devcattimus@gmail.com>
2023-02-27 14:55:30 -0800

    Clean up and document parsing

--- src/parsing.rs
+++ src/parsing.rs
@@ -1,73 +1,92 @@
 use crate::json;
 use json::Value;
 
+//Remove whitespace from a string (except for in quotes)
 fn remove_whitespace(input: &str) -> String {
 	let mut to_return = String::from("");
 
+	//Flag that tells us if we're in a quote block or not
 	let mut in_quote = false;
+	//Previous character (so we can know if there was a \" or not)
 	let mut prev = ' ';
-	for i in 0..input.len() {
-		let cur = input.chars().nth(i).unwrap();
-		match cur {
+
+	//Loop through every character of the array
+	for c in input.chars() {
+		match c {
+			
+			//Ignore whitespace unless it is in a quote block
 			c if c.is_whitespace() => {
 				if in_quote {
-					to_return.push(cur);
+					to_return.push(c);
 				} else {
 					continue;
 				}
 			}
 
-			//make sure we know if we're in a quote or not
+			//Make sure we know if we're in a quote or not
 			'"' => {
 				if prev != '\\' {
 					in_quote = !in_quote;
 				}
 
-				to_return.push(cur);
+				to_return.push(c);
 			}
 
-			_ => {to_return.push(cur);}
+			//Every other character is okay to push directly onto the string
+			_ => {to_return.push(c);}
 		}
 
-		prev = cur;
+		prev = c;
 	}
 
 	return to_return;
 }
 
+//Convert a string to a json::Value object
 pub fn parse_value(value: &str) -> Value {
+
+	//Check what the first character of the string is to see what the value should be
 	let first = value.chars().nth(0).unwrap();
 	match first {
+
+		//Text
 		'"' => {
 			let value = &value[1..value.len()-1];
 			value.into()
 		}
 
+		//Object
 		'{' => {
 			parse_object(value)
 		}
 
+		//Array
 		'[' => {
 			parse_array(value)
 		}
 
+		//Boolean (true/false)
 		't' | 'f' => {
 			Value::Bool(value == "true")
 		}
 
+		//Null
 		'n' => {
 			if value == "null" {
 				return Value::Null
 			}
 
+			//Improperly formatted json will break in this way.
 			println!("Value is n but not null: {}", value);
 			Value::Invalid
 		}
 
+		//Number
 		c if c.is_numeric() => {
 			Value::Number(value.parse().unwrap())
 		}
 
+		//Miscellaneous invalid text
 		_ => {
 			println!("Invalid value: {}", value);
 			Value::Invalid
@@ -75,6 +94,7 @@ pub fn parse_value(value: &str) -> Value {
 	}
 }
 
+//Parse a keypair into a (key, value) tuple
 fn parse_keypair(keypair: &str) -> (&str, Value) {
 
 	//split keypair value
@@ -89,53 +109,65 @@ fn parse_keypair(keypair: &str) -> (&str, Value) {
 	(key, parse_value(value))
 }
 
+//Get a list of where we should split a string
 fn get_splits(input: &str) -> Option<Vec<usize>> {
-	//get split indexes for current object
-	let mut in_quote = false;
-	let mut obj_level = 0;
-	let mut arr_level = 0;
+
+	//The list of indexes we should split on
 	let mut split_indexes: Vec<usize> = Vec::new();
+
+	//Are we in a quote block or not
+	let mut in_quote = false;
+
+	//How many objects deep are we
+	let mut obj_level = 0;
+
+	//How many arrays deep are we
+	let mut arr_level = 0;
+	
+	//Previous character (for '\"')
 	let mut prev: char = ' ';
+
+	//Loop through every character in the string. We need the index to push to the list.
 	for i in 0..input.len() {
 		let cur = input.chars().nth(i).unwrap();
 
 		match cur {
-			//quote start/end
+			//Quote start/end
 			'"' => {
 				if prev != '\\' {
 					in_quote = !in_quote;
 				}
 			}
 			
-			//object start
+			//Object start
 			'{' => {
 				if !in_quote {
 					obj_level += 1;
 				}
 			}
 			
-			//object end
+			//Object end
 			'}' => {
 				if !in_quote {
 					obj_level -= 1;
 				}
 			}
 
-			//array start
+			//Array start
 			'[' => {
 				if !in_quote {
 					arr_level += 1;
 				}
 			}
 
-			//array end
+			//Array end
 			']' => {
 				if !in_quote {
 					arr_level -= 1;
 				}
 			}
 
-			//new split index for the current object
+			//New split index for the current object
 			',' => {
 				if (obj_level + arr_level == 0) && !in_quote {
 					split_indexes.push(i);
@@ -148,39 +180,38 @@ fn get_splits(input: &str) -> Option<Vec<usize>> {
 		prev = cur;
 	}
 
+	//We know that if these conditions are not met, the JSON object is invalid
 	if obj_level != 0 || arr_level != 0 || in_quote {
 		println!("JSON: Split was unable to properly parse value.");
-		println!("Obj level: {}", obj_level);
-		println!("Arr level: {}", arr_level);
-		println!("In quote: {}", in_quote);
-		println!("Input value: {}", input);
 		return None;
 	}
 
 	return Some(split_indexes);
 }
 
+//Parse a JSON object from a string
 pub fn parse_object(input: &str) -> Value {
 	let mut to_return = Value::obj();
 
-	//we know if the first character isn't an opening bracket, our json object is invalid
+	//We know if the first character isn't an opening bracket, our json object is invalid
 	if input.chars().nth(0).unwrap() != '{' {
 		println!("Object does not start with {{");
 		return Value::Invalid;
 	}
 
-	//get split indexes
+	//Remove whitespace and {} from our object to make parsing easier.
 	let trimmed = remove_whitespace(input);
 	let trimmed_len = trimmed.len();
 	let trimmed = &trimmed[1..trimmed_len-1];
 
+	//Get list of split indexes
 	let split_indexes = get_splits(trimmed);
 	if split_indexes.is_none() {
 		return Value::Invalid;
 	}
 	let split_indexes = split_indexes.unwrap();
 
-	//split along all key:value pairs
+	//Split along all key:value pairs
 	let mut prev_index = 0;
 	let mut keypairs: Vec<&str> = Vec::new();
 	for i in split_indexes {
@@ -193,48 +224,50 @@ pub fn parse_object(input: &str) -> Value {
 	}
 	keypairs.push(trimmed.split_at(prev_index).1);
 
-	//remove ,
+	//Remove ,
 	for i in 1..keypairs.len() {
 		keypairs[i] = &keypairs[i][1..];
 	}
 
-	//parse keypairs
+	//Parse all keypairs
 	for pair in keypairs {
 		let result = parse_keypair(pair);
 
-		//some part of the data is invalid
+		//Some part of the data is invalid
 		if let Value::Invalid = result.1 {
 			return Value::Invalid;
 		}
 
-		//assign value
 		to_return[result.0] = result.1;
 	}
 
 	return to_return;
 }
 
+
+//Parse a string into an array
 pub fn parse_array(input: &str) -> Value{
 	let mut to_return = Value::arr();
 	
-	//invalid array
+	//We know if the first character of our array isn't [, it is invalid.
 	if input.chars().nth(0).unwrap() != '[' {
 		println!("JSON: first character of array is not [.");
 		return Value::Invalid;
 	}
 
-	//get split indexes
+	//Trim whitespace and [] from input
 	let trimmed = remove_whitespace(input);
 	let trimmed_len = trimmed.len();
 	let trimmed = &trimmed[1..trimmed_len-1];
 
+	//Get split indexes
 	let split_indexes = get_splits(trimmed);
 	if split_indexes.is_none() {
 		return Value::Invalid;
 	}
 	let split_indexes = split_indexes.unwrap();
 
-	//split into value strings
+	//Split into value strings
 	let mut prev_index = 0;
 	let mut values: Vec<&str> = Vec::new();
 	for i in split_indexes {
@@ -244,12 +277,12 @@ pub fn parse_array(input: &str) -> Value{
 	}
 	values.push(trimmed.split_at(prev_index).1);
 
-	//remove ,
+	//Remove ,
 	for i in 1..values.len() {
 		values[i] = &values[i][1..];
 	}
 
-	//parse values
+	//Parse values
 	for value in values {
 		let text = remove_whitespace(value);
 		let val = parse_value(text.as_str());
