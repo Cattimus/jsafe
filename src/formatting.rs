e343012e7f489e5af257ccc3225a3baa34cfdac5
Cattimus <devcattimus@gmail.com>
2023-02-27 16:14:57 -0800

    Cleanup and refactor formatting

--- src/formatting.rs
+++ src/formatting.rs
@@ -1,31 +1,49 @@
 use crate::json;
 use json::Value;
 
-fn indent(input: &mut String, tab_width: usize, indent_level: usize, spaces: bool) {
+//Helper function to make indenting easier
+fn indent(input: &mut String, indent_level: usize, spaces: bool) {
 	for _ in 0..indent_level {
 		if spaces {
-			for _ in 0..tab_width {
-				input.push(' ');
-			}
+			input.push(' ');
 		} else {
 			input.push('\t');
 		}
 	}
 }
 
-//assumes spaces have been removed
-pub fn format(to_print: Value, tab_width: usize, spaces: bool) -> String {
+//Format a json object from condensed text to human-readable format.
+pub fn format(to_print: Value, spaces: usize) -> String {
 	let mut to_return = String::from("");
 
-	//convert value to string
-	let temp = to_print.to_string();
+	//Check if the object we are trying to format is an object or array
+	match to_print {
+		Value::Object(_) => (),
+		Value::Array(_) => (),
 
+		//If it isn't, return the object converted to a string
+		_ => {return to_print.to_string();}
+	}
 
+	//Convert value to string
+	let obj_str = to_print.to_string();
+	//Whether we're in a quote or not
 	let mut in_quote = false;
+	//Previous character
 	let mut prev = ' ';
+	//How many tabs/spaces to put before a line
 	let mut indent_level = 0;
-	for c in temp.chars() {
+
+	//Calculate how big a tab step should be
+	let mut tab_width = 1;
+	if spaces > 0 {
+		tab_width = spaces;
+	}
+
+	for c in obj_str.chars() {
 		match c {
+
+			//Text block
 			'"' => {
 				if prev != '\\' {
 					in_quote = !in_quote;
@@ -34,54 +52,60 @@ pub fn format(to_print: Value, tab_width: usize, spaces: bool) -> String {
 				to_return.push(c);
 			}
 
+			//Object start
 			'{' => {
 				if !in_quote {
-					indent_level += 1;
+					indent_level += tab_width;
 					to_return.push(c);
 					to_return.push('\n');
 
-					indent(&mut to_return, tab_width, indent_level, spaces);
+					indent(&mut to_return, indent_level, spaces > 0);
 				}
 			}
 
+			//Object end
 			'}' => {
 				if !in_quote {
-					indent_level -= 1;
+					indent_level -= tab_width;
 					to_return.push('\n');
-					indent(&mut to_return, tab_width, indent_level, spaces);
+					indent(&mut to_return, indent_level, spaces > 0);
 					to_return.push(c);
 					
 				}
 			}
 
+			//Array Start
 			'[' => {
 				if !in_quote {
-					indent_level += 1;
+					indent_level += tab_width;
 					to_return.push(c);
 					to_return.push('\n');
 
-					indent(&mut to_return, tab_width, indent_level, spaces);
+					indent(&mut to_return, indent_level, spaces > 0);
 				}
 			}
 
+			//Array end
 			']' => {
 				if !in_quote {
-					indent_level -= 1;
+					indent_level -= tab_width;
 					to_return.push('\n');
-					indent(&mut to_return, tab_width, indent_level, spaces);
+					indent(&mut to_return, indent_level, spaces > 0);
 					to_return.push(c);
 					
 				}
 			}
 
+			//Value separator
 			',' => {
 				if !in_quote {
 					to_return.push(c);
 					to_return.push('\n');
-					indent(&mut to_return, tab_width, indent_level, spaces);
+					indent(&mut to_return, indent_level, spaces > 0);
 				}
 			}
 
+			//Generic character
 			_ => {to_return.push(c);}
 		}
 
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ mod formatting;
 use std::env;
 
 fn main() {
-	env::set_var("RUST_BACKTRACE", "1");
-
     let val = std::fs::read_to_string("test.json").unwrap();
 
 	let obj = parsing::parse_object(val.as_str());
-	println!("{}", formatting::format(obj, 1, false));
+	println!("{}", formatting::format(obj, 2));
 }
