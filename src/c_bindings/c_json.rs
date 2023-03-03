4cdbd5ad60630f88d83c67274504f84ba76d7eed
Cattimus <devcattimus@gmail.com>
2023-03-02 17:02:17 -0800

    Fully switched over to new allocation/reference system

--- src/c_bindings/c_json.rs
+++ src/c_bindings/c_json.rs
@@ -16,6 +16,42 @@ pub struct object {
 //tell rust that we are only referencing values in our struct
 unsafe impl Send for object {}
 
+//Helper function to add a new object to the global pool
+fn create_object(to_add: Value) -> *mut object {
+	let mut val = Box::new(object {
+		root: to_add,
+		current: null_mut()
+	});
+
+	//initialize current
+	val.current = &mut val.root;
+
+	//get a pointer to our object
+	let to_return: *mut object = val.as_mut();
+
+	//append object to our array
+	OBJECTS.lock().unwrap().push(val);
+
+	//send our object to C
+	return to_return;
+}
+
+//Helper function to initialize a new string to the global pool
+fn create_string(to_add: CString) -> *mut CString {
+	let mut val = Box::new(to_add);
+
+	let to_return: *mut CString = val.as_mut();
+
+	STRINGS.lock().unwrap().push(val);
+
+	return to_return;
+}
+
+//Helper function to check if an object is null
+unsafe fn object_is_null(this: *mut object) -> bool{
+	this.is_null() || (*this).current.is_null()
+}
+
 //Free a value
 #[no_mangle]
 pub unsafe extern "C" fn jsafe_free_value(this: *mut object) {
@@ -62,37 +98,6 @@ pub extern "C" fn jsafe_cleanup() {
 	let mut strings = STRINGS.lock().unwrap();
 	strings.clear();
 	strings.shrink_to(0);
-}
-
-//Helper function to add a new object to the global pool
-fn create_object(to_add: Value) -> *mut object {
-	let mut val = Box::new(object {
-		root: to_add,
-		current: null_mut()
-	});
-
-	//initialize current
-	val.current = &mut val.root;
-
-	//get a pointer to our object
-	let to_return: *mut object = val.as_mut();
-
-	//append object to our array
-	OBJECTS.lock().unwrap().push(val);
-
-	//send our object to C
-	return to_return;
-}
-
-//Helper function to initialize a new string to the global pool
-fn create_string(to_add: CString) -> *mut CString {
-	let mut val = Box::new(to_add);
-
-	let to_return: *mut CString = val.as_mut();
-
-	STRINGS.lock().unwrap().push(val);
-
-	return to_return;
 }
 
 //Return a new globally allocated object
@@ -146,83 +151,63 @@ pub extern "C" fn jsafe_new_num(val: c_double) -> Box<Value> {
 
 //Add a new value to an array. This will take ownership of the pointer.
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_add(this: *mut Value, to_add: Option<Box<Value>>) -> *mut Value {
-	if this.is_null() {
-		return null_mut();
+pub unsafe extern "C" fn jsafe_add(this: *mut object, to_add: Option<Box<Value>>) {
+	if object_is_null(this) {
+		return;
 	}
 
 	//shorthand to add a null if you run jsafe_add(val, NULL)
 	if to_add.is_none() {
-		this.as_mut().unwrap().add(Value::Null);
+		(*(*this).current).add(Value::Null);
 	}
 
 	//add item to list
-	let val = this.as_mut().unwrap();
-	val.add(*to_add.unwrap());
-
-	//get item from list
-	let len = val.len() -1;
-	val[len].as_mut()
+	(*(*this).current).add(*to_add.unwrap());
 }
 
 //Pre-allocate slots for the container (to speed up adding values)
 #[no_mangle]
 pub unsafe extern "C" fn jsafe_prealloc(this: *mut object, amount: usize) {
-	if this.is_null() {
+	if object_is_null(this) {
 		return;
 	}
 
-	this.as_mut().unwrap().current.as_mut().unwrap().pre_alloc(amount);
+	(*(*this).current).pre_alloc(amount);
 }
 
 //Get a pointer to a Value from a string index
 #[no_mangle]
 pub unsafe extern "C" fn jsafe_get_property(this: *mut object, key: *const c_char) {
-	if this.is_null() || key.is_null() {
+	if  object_is_null(this) {
 		return;
 	}
-
-	let this = this.as_mut().unwrap();
-	if this.current.is_null() {
-		return;
-	}
-
 
 	let str = CStr::from_ptr(key).to_str().unwrap();
 
 	//replace current with new value
-	this.current = this.current.as_mut().unwrap()[str].as_mut();
+	(*this).current = (*(*this).current)[str].as_mut();
 }
 
 //Set a value from string index. This will free the value passed to it
 #[no_mangle]
 pub unsafe extern "C" fn jsafe_set_property(this: *mut object, key: *const c_char, val: Option<Box<Value>>) {
-	if this.is_null() || key.is_null() {
-		return;
-	}
-
-	let this = this.as_mut().unwrap();
-	if this.current.is_null() {
+	if object_is_null(this) {
 		return;
 	}
 
 	//accounting for NULL
 	let str = CStr::from_ptr(key).to_str().unwrap();
 	if val.is_none() {
-		this.current.as_mut().unwrap()[str] = Value::Null;
+		(*(*this).current)[str] = Value::Null;
 	} else {
-		this.current.as_mut().unwrap()[str] = *val.unwrap();
+		(*(*this).current)[str] = *val.unwrap();
 	}
 }
 
 //Get a pointer to a value from a number index
 #[no_mangle]
 pub unsafe extern "C" fn jsafe_get_index(this: *mut object, key: usize) {
-	if this.is_null() {
-		return;
-	}
-
-	if (*this).current.is_null() {
+	if object_is_null(this) {
 		return;
 	}
 
@@ -236,11 +221,7 @@ pub unsafe extern "C" fn jsafe_get_index(this: *mut object, key: usize) {
 //Return a string representation of an object
 #[no_mangle]
 pub unsafe extern "C" fn jsafe_to_string(this: *mut object) -> *mut CString {
-	if this.is_null() {
-		return create_string(CString::new("Null").unwrap());
-	}
-
-	if (*this).current.is_null() {
+	if object_is_null(this) {
 		return create_string(CString::new("Null").unwrap());
 	}
 
@@ -251,7 +232,7 @@ pub unsafe extern "C" fn jsafe_to_string(this: *mut object) -> *mut CString {
 //Get the length of a json array/object
 #[no_mangle]
 pub unsafe extern "C" fn jsafe_get_len(this: *mut object) -> c_uint {
-	if this.is_null() {
+	if object_is_null(this) {
 		return 0;
 	}
 
@@ -260,37 +241,37 @@ pub unsafe extern "C" fn jsafe_get_len(this: *mut object) -> c_uint {
 
 //Check if a json object has a key
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_has_key(this: *mut Value, key: *const c_char) -> c_int {
-	if this.is_null() || key.is_null() {
+pub unsafe extern "C" fn jsafe_has_key(this: *mut object, key: *const c_char) -> c_int {
+	if object_is_null(this) || key.is_null() {
 		return 0;
 	}
 
 	let str = CStr::from_ptr(key).to_str().unwrap();
-	this.as_ref().unwrap().has(str) as c_int
+	(*(*this).current).has(str) as c_int
 }
 
 //Get a string value from an object
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_get_text(this: *mut Value) -> Option<Box<CString>> {
-	if this.is_null() {
-		return None;
+pub unsafe extern "C" fn jsafe_get_text(this: *mut object) -> *mut CString {
+	if object_is_null(this) {
+		return null_mut();
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
-		Value::Text(x) => Some(Box::new(CString::new(x.as_bytes()).unwrap())),
-		_ => None
+		Value::Text(x) => create_string(CString::new(x.as_bytes()).unwrap()),
+		_ => null_mut()
 	}
 }
 
 //Get a number value from an object
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_get_num(this: *mut Value) -> c_double {
-	if this.is_null() {
+pub unsafe extern "C" fn jsafe_get_num(this: *mut object) -> c_double {
+	if object_is_null(this) {
 		return 0.0;
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
 		Value::Number(x) => *x,
 		_ => 0.0
@@ -299,12 +280,12 @@ pub unsafe extern "C" fn jsafe_get_num(this: *mut Value) -> c_double {
 
 //Get a number value from an object
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_get_bool(this: *mut Value) -> c_int {
-	if this.is_null() {
+pub unsafe extern "C" fn jsafe_get_bool(this: *mut object) -> c_int {
+	if object_is_null(this) {
 		return 0;
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
 		Value::Bool(x) => *x as c_int,
 		_ => 0
@@ -313,12 +294,12 @@ pub unsafe extern "C" fn jsafe_get_bool(this: *mut Value) -> c_int {
 
 //Check if a value is null
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_is_null(this: *mut Value) -> c_int {
-	if this.is_null() {
+pub unsafe extern "C" fn jsafe_is_null(this: *mut object) -> c_int {
+	if object_is_null(this) {
 		return 0;
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
 		Value::Null => 1,
 		_ => 0
@@ -327,12 +308,12 @@ pub unsafe extern "C" fn jsafe_is_null(this: *mut Value) -> c_int {
 
 //Check if a value is invalid
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_is_valid(this: *mut Value) -> c_int {
-	if this.is_null() {
+pub unsafe extern "C" fn jsafe_is_valid(this: *mut object) -> c_int {
+	if object_is_null(this) {
 		return 0;
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
 		Value::Invalid => 0,
 		_ => 1
@@ -341,12 +322,12 @@ pub unsafe extern "C" fn jsafe_is_valid(this: *mut Value) -> c_int {
 
 //Check if a value is text
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_is_text(this: *mut Value) -> c_int {
-	if this.is_null() {
+pub unsafe extern "C" fn jsafe_is_text(this: *mut object) -> c_int {
+	if object_is_null(this) {
 		return 1;
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
 		Value::Text(_) => 1,
 		_ => 0
@@ -355,12 +336,12 @@ pub unsafe extern "C" fn jsafe_is_text(this: *mut Value) -> c_int {
 
 //Check if a value is a number
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_is_num(this: *mut Value) -> c_int {
-	if this.is_null() {
+pub unsafe extern "C" fn jsafe_is_num(this: *mut object) -> c_int {
+	if object_is_null(this) {
 		return 1;
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
 		Value::Number(_) => 1,
 		_ => 0
@@ -369,12 +350,12 @@ pub unsafe extern "C" fn jsafe_is_num(this: *mut Value) -> c_int {
 
 //Check if a value is an object
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_is_obj(this: *mut Value) -> c_int {
-	if this.is_null() {
+pub unsafe extern "C" fn jsafe_is_obj(this: *mut object) -> c_int {
+	if object_is_null(this) {
 		return 1;
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
 		Value::Object(_) => 1,
 		_ => 0
@@ -383,12 +364,12 @@ pub unsafe extern "C" fn jsafe_is_obj(this: *mut Value) -> c_int {
 
 //Check if a value is an array
 #[no_mangle]
-pub unsafe extern "C" fn jsafe_is_arr(this: *mut Value) -> c_int {
-	if this.is_null() {
+pub unsafe extern "C" fn jsafe_is_arr(this: *mut object) -> c_int {
+	if object_is_null(this) {
 		return 1;
 	}
 
-	let val = this.as_ref().unwrap();
+	let val = (*this).current.as_ref().unwrap();
 	match val {
 		Value::Array(_) => 1,
 		_ => 0
