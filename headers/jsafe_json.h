a71044f78fa66ffd07332a85b2f0d2eb6f74767a
Cattimus <devcattimus@gmail.com>
2023-03-02 17:09:00 -0800

    Updated headers to better reflect current API

--- headers/jsafe_json.h
+++ headers/jsafe_json.h
@@ -4,11 +4,12 @@
 #include "jsafe_types.h"
 
 //Deallocation functions
-extern "C" void jsafe_free_value(jsafe_value* val);
+extern "C" void jsafe_free_value(jsafe_object* val);
 extern "C" void jsafe_free_string(jsafe_string* str);
 extern "C" void jsafe_cleanup();
 
 //Allocation functions
+extern "C" jsafe_object* jsafe_new_root();
 extern "C" jsafe_value* jsafe_new_obj();
 extern "C" jsafe_value* jsafe_new_arr();
 extern "C" jsafe_value* jsafe_new_text(const char* text);
@@ -17,25 +18,25 @@ extern "C" jsafe_value* jsafe_new_bool(int val);
 extern "C" jsafe_value* jsafe_new_num(double val);
 
 //Set properties
-extern "C" jsafe_value* jsafe_add(jsafe_value* root, jsafe_value* to_add); //Append to array
-extern "C" jsafe_value* jsafe_get_property(jsafe_value* root, const char* key); //Get for object
-extern "C" jsafe_value* jsafe_set_property(jsafe_value* root, const char* key, jsafe_value* val); //set for object
-extern "C" void jsafe_prealloc(jsafe_value* root, unsigned int size);
+extern "C" void jsafe_add(jsafe_object* root, jsafe_value* to_add); //Append to array
+extern "C" void jsafe_get_property(jsafe_object* root, const char* key); //Get for object
+extern "C" void jsafe_set_property(jsafe_object* root, const char* key, jsafe_value* val); //set for object
+extern "C" void jsafe_prealloc(jsafe_object* root, unsigned int size);
 
 //Get properties
-extern "C" unsigned int jsafe_get_len(jsafe_value* val);
-extern "C" jsafe_string* jsafe_get_text(jsafe_value* val);
-extern "C" double jsafe_get_num(jsafe_value* val);
-extern "C" int jsafe_get_bool(jsafe_value* val);
-extern "C" jsafe_string* jsafe_to_string(jsafe_value* val);
+extern "C" unsigned int jsafe_get_len(jsafe_object* val);
+extern "C" jsafe_string* jsafe_get_text(jsafe_object* val);
+extern "C" double jsafe_get_num(jsafe_object* val);
+extern "C" int jsafe_get_bool(jsafe_object* val);
+extern "C" jsafe_string* jsafe_to_string(jsafe_object* val);
 
 //Check properties
-extern "C" int jsafe_has_key(jsafe_value* val, const char* key);
-extern "C" int jsafe_is_null(jsafe_value* val);
-extern "C" int jsafe_is_valid(jsafe_value* val);
-extern "C" int jsafe_is_text(jsafe_value* val);
-extern "C" int jsafe_is_num(jsafe_value* val);
-extern "C" int jsafe_is_obj(jsafe_value* val);
-extern "C" int jsafe_is_arr(jsafe_value* val);
+extern "C" int jsafe_has_key(jsafe_object* val, const char* key);
+extern "C" int jsafe_is_null(jsafe_object* val);
+extern "C" int jsafe_is_valid(jsafe_object* val);
+extern "C" int jsafe_is_text(jsafe_object* val);
+extern "C" int jsafe_is_num(jsafe_object* val);
+extern "C" int jsafe_is_obj(jsafe_object* val);
+extern "C" int jsafe_is_arr(jsafe_object* val);
 
 #endif
--- headers/jsafe_types.h
+++ headers/jsafe_types.h
@@ -2,6 +2,7 @@
 #define JSAFE_TYPES_H
 
 typedef void jsafe_value;
+typedef void jsafe_object;
 typedef const char* jsafe_string;
 
 #endif
